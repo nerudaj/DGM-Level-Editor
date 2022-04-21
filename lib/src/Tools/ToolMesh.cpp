17cca1cdf1294f0c436209115bf53776d853c8b5
Jakub Neruda <nerudaj@email.cz>
2022-04-21 23:06:03 +0200

    Initialize overlay on load

--- lib/src/Tools/ToolMesh.cpp
+++ lib/src/Tools/ToolMesh.cpp
@@ -3,40 +3,52 @@
 #include "include/LogConsole.hpp"
 #include <filesystem>
 
-void ToolMesh::penClicked(const sf::Vector2i& position) {
+void ToolMesh::penClicked(const sf::Vector2i& position)
+{
 	signalStateChanged();
 
 	auto tilePos = worldToTilePos(position);
-	if (isPositionValid(tilePos)) {
+	if (isPositionValid(tilePos))
+	{
 		tilemap.drawArea(position, position, true, penValue, defaultBlocks[penValue]);
 	}
 }
 
-void ToolMesh::penDragStarted(const sf::Vector2i& start) {
+void ToolMesh::penDragStarted(const sf::Vector2i& start)
+{
 	rectShape.setPosition(sf::Vector2f(start));
 }
 
-void ToolMesh::penDragUpdate(const sf::Vector2i& start, const sf::Vector2i& end) {
-	if (mode == DrawMode::Pencil) {
+void ToolMesh::penDragUpdate(const sf::Vector2i& start, const sf::Vector2i& end)
+{
+	if (mode == DrawMode::Pencil)
+	{
 		penClicked(end);
-	} else {
+	}
+	else
+	{
 		rectShape.setSize(sf::Vector2f(end - start));
 	}
 }
 
-void ToolMesh::penDragEnded(const sf::Vector2i& start, const sf::Vector2i& end) {
+void ToolMesh::penDragEnded(const sf::Vector2i& start, const sf::Vector2i& end)
+{
 	signalStateChanged();
 
-	if (mode == DrawMode::RectEdge) {
+	if (mode == DrawMode::RectEdge)
+	{
 		tilemap.drawArea(start, end, false, penValue, defaultBlocks[penValue]);
-	} else if (mode == DrawMode::RectFill) {
+	}
+	else if (mode == DrawMode::RectFill)
+	{
 		tilemap.drawArea(start, end, true, penValue, defaultBlocks[penValue]);
 	}
 }
 
 void ToolMesh::penDragCancel(const sf::Vector2i&) {}
 
-void ToolMesh::configure(nlohmann::json &config) {
+void ToolMesh::configure(nlohmann::json& config)
+{
 	const std::string TOOL_STR = "toolMesh";
 	const auto rootPath = std::filesystem::path(config["configFolder"].get<std::string>());
 
@@ -53,7 +65,8 @@ void ToolMesh::configure(nlohmann::json &config) {
 
 	defaultBlocks.resize(config[TOOL_STR]["defaultProperties"]["count"]);
 	unsigned i = 0;
-	for (auto &item : config[TOOL_STR]["defaultProperties"]["solids"]) {
+	for (auto& item : config[TOOL_STR]["defaultProperties"]["solids"])
+	{
 		defaultBlocks[i++] = bool(int(item));
 		Log::write(std::to_string(i) + ": " + std::to_string(bool(int(item))));
 	}
@@ -69,11 +82,12 @@ void ToolMesh::configure(nlohmann::json &config) {
 	penHistory.clear();
 }
 
-void ToolMesh::resize(unsigned width, unsigned height) {
+void ToolMesh::resize(unsigned width, unsigned height)
+{
 	signalStateChanged();
 
 	bgr.build({ width, height }, clip.getFrameSize());
-	
+
 	decltype(tilemap) cpy;
 	cpy.init(texture, clip);
 	cpy.mesh.setVoxelSize(clip.getFrameSize());
@@ -81,11 +95,14 @@ void ToolMesh::resize(unsigned width, unsigned height) {
 	cpy.buildAll(width, height, clip.getFrameSize());
 
 	// Copy and center data
-	if (not tilemap.empty() && tilemap.mesh.getDataSize().x <= width && tilemap.mesh.getDataSize().y <= height) {
+	if (not tilemap.empty() && tilemap.mesh.getDataSize().x <= width && tilemap.mesh.getDataSize().y <= height)
+	{
 		auto offset = (sf::Vector2u(width, height) - tilemap.mesh.getDataSize()) / 2u;
 
-		for (unsigned y = 0; y < tilemap.mesh.getDataSize().y; y++) {
-			for (unsigned x = 0; x < tilemap.mesh.getDataSize().x; x++) {
+		for (unsigned y = 0; y < tilemap.mesh.getDataSize().y; y++)
+		{
+			for (unsigned x = 0; x < tilemap.mesh.getDataSize().x; x++)
+			{
 				auto value = tilemap.getTile(x, y);
 				bool blocking = tilemap.getTileBlock(x, y);
 				cpy.setTile(x + offset.x, y + offset.y, value, blocking);
@@ -98,7 +115,8 @@ void ToolMesh::resize(unsigned width, unsigned height) {
 	tilemap.overlay.init();
 }
 
-void ToolMesh::saveTo(LevelD &lvd) {
+void ToolMesh::saveTo(LevelD& lvd)
+{
 	Log::write("ToolMesh::saveTo");
 	lvd.mesh.tileWidth = clip.getFrameSize().x;
 	lvd.mesh.tileHeight = clip.getFrameSize().y;
@@ -108,37 +126,53 @@ void ToolMesh::saveTo(LevelD &lvd) {
 	lvd.mesh.layers.resize(1);
 	lvd.mesh.layers[0].tiles.resize(lvd.mesh.layerWidth * lvd.mesh.layerHeight);
 	lvd.mesh.layers[0].blocks.resize(lvd.mesh.layerWidth * lvd.mesh.layerHeight);
-	for (unsigned tileY = 0, i = 0; tileY < lvd.mesh.layerHeight; tileY++) {
-		for (unsigned tileX = 0; tileX < lvd.mesh.layerWidth; tileX++, i++) {
+	for (unsigned tileY = 0, i = 0; tileY < lvd.mesh.layerHeight; tileY++)
+	{
+		for (unsigned tileX = 0; tileX < lvd.mesh.layerWidth; tileX++, i++)
+		{
 			lvd.mesh.layers[0].tiles[i] = tilemap.getTile(tileX, tileY);
 			lvd.mesh.layers[0].blocks[i] = tilemap.mesh[i];
 		}
 	}
 }
 
-void ToolMesh::loadFrom(const LevelD &lvd) {
+void ToolMesh::loadFrom(const LevelD& lvd)
+{
 	Log::write("ToolMesh::loadFrom");
 	resize(lvd.mesh.layerWidth, lvd.mesh.layerHeight);
 
 	tilemap.build(lvd.mesh, 0);
 	tilemap.mesh = dgm::Mesh(lvd.mesh, 0);
-	tilemap.build(lvd.mesh, 0);
+
+	// init overlay
+	for (unsigned y = 0; y < lvd.mesh.layerHeight; y++)
+	{
+		for (unsigned x = 0; x < lvd.mesh.layerWidth; x++)
+		{
+			if (lvd.mesh.layers[0].blocks[y * lvd.mesh.layerWidth + x])
+				tilemap.overlay.map.changeTile(x, y, 1);
+		}
+	}
 }
 
-void ToolMesh::drawTo(tgui::Canvas::Ptr &canvas, uint8_t) {
+void ToolMesh::drawTo(tgui::Canvas::Ptr& canvas, uint8_t)
+{
 	canvas->draw(tilemap);
 	bgr.draw(canvas);
 
 	if (enableOverlay) canvas->draw(tilemap.overlay.map);
 
-	if (isPenDragging()) {
-		if (mode == DrawMode::RectEdge || mode == DrawMode::RectFill) {
+	if (isPenDragging())
+	{
+		if (mode == DrawMode::RectEdge || mode == DrawMode::RectFill)
+		{
 			canvas->draw(rectShape);
 		}
 	}
 }
 
-ToolProperty &ToolMesh::getProperty() {
+ToolProperty& ToolMesh::getProperty()
+{
 	auto pos = getPenPosition();
 
 	tileProperty.clear();
@@ -157,12 +191,14 @@ ToolProperty &ToolMesh::getProperty() {
 	return tileProperty;
 }
 
-void ToolMesh::setProperty(const ToolProperty &) {
+void ToolMesh::setProperty(const ToolProperty&)
+{
 	tilemap.setTile(tileProperty.tileX, tileProperty.tileY, tileProperty.tileValue, tileProperty.blocking);
 	signalStateChanged();
 }
 
-void ToolMesh::buildCtxMenu(tgui::MenuBar::Ptr &menu) {
+void ToolMesh::buildCtxMenu(tgui::MenuBar::Ptr& menu)
+{
 	Tool::buildCtxMenu(menu); // bootstrap
 
 	const std::string OPTION_PENCIL = "Pencil Mode (Shift+P)";
@@ -170,24 +206,28 @@ void ToolMesh::buildCtxMenu(tgui::MenuBar::Ptr &menu) {
 	const std::string OPTION_EDGE = "Rect-edge Mode (Shift+E)";
 	const std::string OPTION_OVERLAY = "Toggle overlay (Shift+O)";
 
-	addCtxMenuItem(menu, OPTION_PENCIL, [this]() { changeDrawingMode(DrawMode::Pencil); }, sf::Keyboard::P);
-	addCtxMenuItem(menu, OPTION_FILL, [this]() { changeDrawingMode(DrawMode::RectFill); }, sf::Keyboard::F);
-	addCtxMenuItem(menu, OPTION_EDGE, [this]() { changeDrawingMode(DrawMode::RectEdge); }, sf::Keyboard::E);
-	addCtxMenuItem(menu, OPTION_OVERLAY, [this]() { enableOverlay = !enableOverlay; }, sf::Keyboard::O);
+	addCtxMenuItem(menu, OPTION_PENCIL, [this] () { changeDrawingMode(DrawMode::Pencil); }, sf::Keyboard::P);
+	addCtxMenuItem(menu, OPTION_FILL, [this] () { changeDrawingMode(DrawMode::RectFill); }, sf::Keyboard::F);
+	addCtxMenuItem(menu, OPTION_EDGE, [this] () { changeDrawingMode(DrawMode::RectEdge); }, sf::Keyboard::E);
+	addCtxMenuItem(menu, OPTION_OVERLAY, [this] () { enableOverlay = !enableOverlay; }, sf::Keyboard::O);
 }
 
-void ToolMesh::changeDrawingMode(ToolMesh::DrawMode newMode) {
+void ToolMesh::changeDrawingMode(ToolMesh::DrawMode newMode)
+{
 	Log::write("Mesh: Changing draw mode to " + std::to_string(newMode));
 	mode = newMode;
-	if (mode == DrawMode::RectEdge) {
+	if (mode == DrawMode::RectEdge)
+	{
 		rectShape.setFillColor(sf::Color::Transparent);
 	}
-	else if (mode == DrawMode::RectFill) {
+	else if (mode == DrawMode::RectFill)
+	{
 		rectShape.setFillColor(sf::Color(128, 0, 0, 128));
 	}
 }
 
-std::string std::to_string(ToolMesh::DrawMode mode) {
+std::string std::to_string(ToolMesh::DrawMode mode)
+{
 	if (mode == ToolMesh::DrawMode::Pencil) return "Pencil";
 	else if (mode == ToolMesh::DrawMode::RectFill) return "RectFill";
 	else if (mode == ToolMesh::DrawMode::RectEdge) return "RectEdge";
@@ -195,7 +235,8 @@ std::string std::to_string(ToolMesh::DrawMode mode) {
 	return "Error";
 }
 
-void MeshToolProperty::buildModalSpecifics(tgui::ScrollablePanel::Ptr& dst) {
+void MeshToolProperty::buildModalSpecifics(tgui::ScrollablePanel::Ptr& dst)
+{
 	constexpr bool DISABLED = false; // the function accepts predicate enabled
 
 	addOption(dst, "Tile X:", "X coordinate of the tile", tileX, 0, DISABLED);
