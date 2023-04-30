623f9c1a9a3548469a4ccff5d034ee7d4a997b21
Jakub Neruda <nerudaj@email.cz>
2023-04-30 15:18:37 +0200

    Fix tests

--- lib/tests/include/Stubs/FileApiStub.hpp
+++ lib/tests/include/Stubs/FileApiStub.hpp
@@ -5,23 +5,31 @@
  * auto fileApiMock = GC<FileApiStub>();
  * auto fileApiSpy = Mock<FileApiInterface>(*fileApiMock);
  *
- * Use fileApiMock for dependency injection and set expectations on the spy object.
+ * Use fileApiMock for dependency injection and set expectations on the spy
+ * object.
  */
 class FileApiStub final : public FileApiInterface
 {
 public:
-	std::string resolveAppdata() const override
-	{
-		return "";
-	}
+    std::string resolveAppdata() const override
+    {
+        return "";
+    }
 
-	std::optional<std::string> getSaveFileName(const char* filter) const override
-	{
-		return "";
-	}
+    std::optional<std::string>
+    getSaveFileName(const char* filter) const override
+    {
+        return "";
+    }
 
-	std::optional<std::string> getOpenFileName(const char* filter) const override
-	{
-		return "";
-	}
+    std::optional<std::string>
+    getOpenFileName(const char* filter) const override
+    {
+        return "";
+    }
+
+    std::optional<std::filesystem::path> selectFolder() const override
+    {
+        return {};
+    }
 };
--- lib/tests/src/ProcessCreator.cpp
+++ lib/tests/src/ProcessCreator.cpp
@@ -1,24 +1,28 @@
+#include "TestHelpers/TestAssets.hpp"
 #include <catch.hpp>
 #include <include/Utilities/ProcessCreator.hpp>
-
-#include "TestHelpers/TestAssets.hpp"
+#include <iostream>
 
 TEST_CASE("[ProcessCreator]")
 {
-	ProcessCreator creator;
+    ProcessCreator creator;
 
-	SECTION("Correctly executes existing batfile")
-	{
-		auto result = creator.Exec(Process::PROCESS_PATH.string(), "param1 param2");
-		REQUIRE(result.has_value());
-	}
+    SECTION("Correctly executes existing batfile")
+    {
+        auto result =
+            creator.Exec(Process::PROCESS_PATH, "param1 param2", "C:\\");
+        REQUIRE(result.has_value());
+    }
 
-	SECTION("Returns error message if batfile doesn't exist")
-	{
-		auto result = creator.Exec("nonexistent.bat", "param1");
+    SECTION("Returns error message if batfile doesn't exist")
+    {
+        auto result = creator.Exec("nonexistent.bat", "param1", "C:\\");
 
-		REQUIRE_FALSE(result.has_value());
-		// There are extra whitespaces, so direct comparison is not ideal
-		REQUIRE(result.error().find("The system cannot find the file specified.") != std::string::npos);
-	}
+        REQUIRE_FALSE(result.has_value());
+        std::cout << result.error() << std::endl;
+        // There are extra whitespaces, so direct comparison is not ideal
+        REQUIRE(
+            result.error().find("The system cannot find the file specified.")
+            != std::string::npos);
+    }
 }
